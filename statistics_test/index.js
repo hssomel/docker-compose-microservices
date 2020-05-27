87209273556a403cf33ec4eace6fa8b53b79a02a
Gurkamal Singh <info@gurkamal.io>
2020-05-27 00:59:05 -0700

    manage HAProxy routing

--- statistics_test/index.js
+++ statistics_test/index.js
@@ -1,52 +1,34 @@
 const express = require("express");
 const path = require("path");
-const dns = require("dns");
+const app = express();
 
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
-const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
-app.use("/statistics/assets", express.static(path.join(__dirname, "assets")));
 
 // Config
 const expressPort = process.env.PORT || 3000;
 const user = encodeURIComponent(`${process.env.MONGO_USERNAME}`);
 const password = encodeURIComponent(`${process.env.MONGO_PASSWORD}`);
 const authMechanism = "DEFAULT";
-const mongoEndpoint = `${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}`;
-const url = `mongodb://${user}:${password}@${mongoEndpoint}/?authMechanism=${authMechanism}`;
+const mongoHost = `${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}`;
+const url = `mongodb://${user}:${password}@${mongoHost}/?authMechanism=${authMechanism}`;
 const dbName = "admin";
 
-// Dynamically get DNS of mongo
-async function lookupPromise() {
-  return new Promise((resolve, reject) => {
-    dns.lookup(`${process.env.MONGO_HOSTNAME}`, (err, address, family) => {
-      if (err) reject(err);
-      resolve(address);
-    });
-  });
-}
-
 // When a request comes in, connect to MongoDB and respond with statistics
-app.get("/statistics", (req, res) => {
+app.get("/", (req, res) => {
   let response = [];
   MongoClient.connect(url, async function (err, client) {
-    const mongoInternalIP = await lookupPromise();
-    console.log(
-      `Connected to MongoDB at ${mongoInternalIP}:${process.env.MONGO_PORT}`
-    );
+    console.log(`Connected to MongoDB at ${mongoHost}`);
     const adminDb = client.db(dbName).admin();
     const databases = await adminDb.listDatabases();
     const users = await adminDb.command({ usersInfo: { forAllDBs: true } });
     const serverInfo = await adminDb.serverInfo();
-    res.render("index", {
-      users,
-      databases,
-      serverInfo,
-      mongoInternalIP,
-    });
+    console.log(serverInfo);
+    res.send(serverInfo);
+    // res.render("index");
     client.close();
   });
 });
