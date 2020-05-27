03afb7233ba7bf1bc86e5daa80f4a779958c8fbf
Gurkamal Singh <info@gurkamal.io>
2020-05-27 04:46:03 -0700

    use full path with module

--- static/index.js
+++ static/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const path = require("path");
 
 // Constants
 const PORT = 8080;
@@ -8,7 +9,7 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.use("/", express.static("public"));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
