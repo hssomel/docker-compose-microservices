dee85a9562a696ea21fee3ab456779a129aec368
Gurkamal Singh <info@gurkamal.io>
2020-05-26 22:00:53 -0700

    statistics methods resolve correctly

--- statistics/index.js
+++ statistics/index.js
@@ -1,21 +1,31 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
 
+// MongoDB Connection Config
 const user = encodeURIComponent(`${process.env.MONGO_USERNAME}`);
 const password = encodeURIComponent(`${process.env.MONGO_PASSWORD}`);
 const authMechanism = "DEFAULT";
-
-// Connection URL
 const mongoHost = `${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}`;
 const url = `mongodb://${user}:${password}@${mongoHost}/?authMechanism=${authMechanism}`;
+const dbName = "admin";
 
-// Create a new MongoClient
-const client = new MongoClient(url);
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-  assert.equal(null, err);
-  console.log(`Connected to MongoDB at ${mongoHost}`);
-
-  // client.close();
+// When a request comes in, connect to MongoDB and respond with statistics
+app.get("/", (req, res) => {
+  let response = [];
+  MongoClient.connect(url, async function (err, client) {
+    const adminDb = client.db(dbName).admin();
+    response.push(await adminDb.listDatabases());
+    response.push(await adminDb.command({ usersInfo: { forAllDBs: true } }));
+    response.push(await adminDb.serverInfo());
+    res.send(response);
+    client.close();
+  });
 });
+
+app.listen(port, () =>
+  console.log(`MongoDB Statistics App listening at http://localhost:${port}`)
+);
